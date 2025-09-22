9cae646459ae010745f854b95755f32730617ff3
Liu-Weijian <742579725@qq.com>
2025-09-22 23:17:12 +0800

    第二十六天

--- webook/pkg/ginx/middleware/ratelimit/builder.go
+++ webook/pkg/ginx/middleware/ratelimit/builder.go
@@ -38,16 +38,16 @@ func (b *Builder) Prefix(prefix string) *Builder {
 func (b *Builder) Build() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		limited, err := b.limit(ctx)
-		//if err != nil {
-		//	log.Println(err)
-		//	// 这一步很有意思，就是如果这边出错了
-		//	// 要怎么办？
-		//	// 保守做法：因为借助于 Redis 来做限流，那么 Redis 崩溃了，为了防止系统崩溃，直接限流
-		//	ctx.AbortWithStatus(http.StatusInternalServerError)
-		//	// 激进做法：虽然 Redis 崩溃了，但是这个时候还是要尽量服务正常的用户，所以不限流
-		//	// ctx.Next()
-		//	return
-		//}
+		if err != nil {
+			log.Println(err)
+			// 这一步很有意思，就是如果这边出错了
+			// 要怎么办？
+			// 保守做法：因为借助于 Redis 来做限流，那么 Redis 崩溃了，为了防止系统崩溃，直接限流
+			ctx.AbortWithStatus(http.StatusInternalServerError)
+			// 激进做法：虽然 Redis 崩溃了，但是这个时候还是要尽量服务正常的用户，所以不限流
+			// ctx.Next()
+			return
+		}
 		if limited {
 			log.Println(err)
 			ctx.AbortWithStatus(http.StatusTooManyRequests)
